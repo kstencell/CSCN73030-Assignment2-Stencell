d05fcf46e02095664f7ac4ae5724f77894da84c0
Karl Stencell <karldstencell@gmail.com>
2022-09-22 14:14:22 -0400

    read from StudentData.txt and populate a vector space with student structs

--- CSCN73030-Assignment2-Stencell/Source.cpp
+++ CSCN73030-Assignment2-Stencell/Source.cpp
@@ -1,8 +1,40 @@
 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "Student.h"
 
-
+using namespace std;
 
 int main(void) {
+
+	// open file and double check
+	ifstream studentData("StudentData.txt");
+	if (!studentData) {
+		cout << "Error reading file" << endl;
+		exit(1);
+	}
+
+	vector<STUDENT_DATA> students;
+	string line;
+
+	// read one line at a time from the .txt file
+	while (getline(studentData, line)) {
+		STUDENT_DATA newStudent;
+		stringstream ss(line);
+		
+		// parse the lines by comma and assign struct variables accordingly
+		string substr;
+		getline(ss, substr, ',');
+		newStudent.lastName = substr;
+		getline(ss, substr); // stringstream is pushed forward by last getline() so no need to specify comma here, it goes to end of line by default
+		newStudent.firstName = substr;
+
+		// add to vector of students
+		students.push_back(newStudent);
+	}
 
 	return 0;
 }
---
+++ CSCN73030-Assignment2-Stencell/Student.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+typedef struct STUDENT_DATA {
+
+	string firstName;
+	string lastName;
+
+}STUDENT_DATA;
